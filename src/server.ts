69c8f6de824898902fea863e764ce0e38a64e930
Marcos <marcos.oliveira.developer@gmail.com>
2023-08-09 15:41:39 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,31 @@
 import "reflect-metadata";
-import express from "express";
-import { AppDataSource } from "./data-source";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
+import { router } from "./routes";
 
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!");
+import "./database";
 
-        const app = express();
+const app = express();
+app.use(cors());
 
-        app.use(express.json());
+app.use(express.json());
 
-        app.listen(3000, ()=> console.log("Server esta rodando"));
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err);
+app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
     });
+  }
+);
 
-
-
-
+app.listen(3000, () => console.log("Server is running"));
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,28 +1,43 @@
-import { UserRepositories } from "../repositories/UserRepositories";
-
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
-    name: string;
-    email: string;
-    admin?: boolean;
+  name: string;
+  email: string;
+  admin?: boolean;
+  password: string;
 }
 
-export class CreateUserService{
-    async execute({ name, email, admin } : IUserRequest){
-        
-        if(!email){
-            throw new Error("Email Incorreto");
-        }
+class CreateUserService {
+  async execute({ name, email, admin = false, password }: IUserRequest) {
+    const usersRepository = getCustomRepository(UsersRepositories);
 
-        const userAlreadyExists = await UserRepositories.findOneBy({ email:email });
-
-        if(userAlreadyExists){
-            throw new Error("Usuario ja existe");
-        }
-
-        const user = await UserRepositories.create({ name,email,admin });  
-        
-        return user;
+    if (!email) {
+      throw new Error("Email incorrect");
     }
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+      password: passwordHash,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
 }
 
+export { CreateUserService };
